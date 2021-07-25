ded34561206b0ffcdb64b148cb3084f8df78cec0
wangbo <bbw1996@qq.com>
2021-07-26 00:09:50 +0800

    使用next-images插件

--- next.config.js
+++ next.config.js
@@ -4,24 +4,9 @@
  *@Description:
  *@Date 2021/7/16
  */
-module.exports = {
-  webpack: (config, options) => {
-    const isServer = options.isServer // 判断是否是node环境
-    config.module.rules.push({
-      test: /\.(png|jpg|jpeg|gif|svg)$/,
-      test: /\.jpg$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[contenthash].[ext]',
-            outputPath: 'static', // 硬盘路径
-            publicPath: '/_next/static' // 网站路径
-          }
-        }
-      ],
-    })
-
+const withImages = require('next-images')
+module.exports = withImages({
+  webpack(config, options) {
     return config
-  },
-}
+  }
+})
