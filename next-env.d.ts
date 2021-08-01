3115418012fbb517027d62fef347da55c2d058a0
wangbo <bbw1996@qq.com>
2021-08-01 20:16:58 +0800

    写了第一个接口

---
+++ next-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
